aaa90bbb8c29f433b6bdb80a3557e9e41a029475
azambhrgn <azambhrgn@gmail.com>
2018-03-31 00:22:13 +0530

    "Tree programs"

--- Practice2018/src/main/java/com/main/Practice2018/Application.java
+++ Practice2018/src/main/java/com/main/Practice2018/Application.java
@@ -12,12 +12,42 @@ public class Application {
 	public static void main(String[] args) {
 		//ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-		System.out.println("Hello");
 		Tree root = tree.insert(null, 10);
-		root = tree.insert(root, 5);
 		root = tree.insert(root, 15);
+		root = tree.insert(root, 25);
 		root = tree.insert(root, 20);
+		root = tree.insert(root, 8);
+		root = tree.insert(root, 5);
+		root = tree.insert(root, 30);
 
-		tree.display(root);
+		/*
+		 * Choice for ordering
+		 * 1- PreOrder
+		 * 2- PostOrder
+		 * 3- Inorder
+		*/
+		//tree.display(root,1);
+		System.out.println("*************************");
+		/*tree.display(root,2);
+		System.out.println("*************************");
+		tree.display(root,3);*/
+		
+		
+		//height of the tree
+		System.out.println("Height " + tree.height(root));
+		
+		System.out.println("#############");
+		tree.levelOrder(root);
+		
+		System.out.println("****K distance from root**");
+		tree.kDistancefromRoot(root, 2);
+		
+		System.out.println("****Get Level of given node*");
+		int num = 30;
+		int l = tree.getLevelOfNode(root, num, 0);
+		System.out.println((l == 0 && root.num != num) ? "Not found" : l);
+
+		System.out.println("******print ancestor of node****");
+		tree.printAncestor(root, num);
 	}
 }
